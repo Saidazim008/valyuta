08f8654d166aa99e816a572cc6b6f2c9221fe483
Saidazim008 <saidazibobomurodov@gmail.com>
2026-04-13 18:00:44 +0500

    hamma qismi tayyor

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,17 @@
-import React, { useState } from 'react';
-import { Routes, Route, useNavigate } from "react-router-dom";
-import Home from './home/Home';
-import NotePage from './num/NotePage';
-import './App.css';
+import React from 'react'
+import "./style./StyleComponent"
+// import { BrowserRouter as Router, Routes, Route useNavigate } from 'react-router-dom';
+import Home from './pages/home./Home.jsx';
+import { useState } from 'react';
 
 const App = () => {
-
-  const navigation = useNavigate();
-
-  const [active, setActive] = useState(false);
-
   return (
-    <div className="app-container">
+    <div>
+    <Home />  
 
-      <h1>Hello my library</h1>
 
-      <div className="button-group">
-
-        <button onClick={() => setActive(true)}>Modalni ochish</button>
-
-        <button onClick={() => navigation("/home")}>Click me</button>
-      </div>
-
-      {active && (
-        <div className="modal-overlay">
-          <div className="modal-content">
-            <h3>Modal </h3>
-            <p>Salom Hammaga</p>
-
-            <button className="close-btn" onClick={() => setActive(false)}>
-              Yopish
-            </button>
-          </div>
-        </div>
-      )}
-
-      {/* Sahifa almashganda aynan mana shu qism o'zgaradi */}
-      <Routes>
-
-        {/* Asosiy kirish qismi */}
-        <Route path="/" element={<Home />} />
-
-        {/* Home sahifasi - Agar kamentga olinsa pastdagi NotePage chiqadi */}
-        <Route path="/home" element={<Home />} />
-
-        {/* Noto'g'ri manzil yozilsa yoki yuqoridagilar o'chirilsa ishlaydi */}
-        <Route path="*" element={<NotePage />} />
-
-      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
