ec573f4faf45557b5689ca63242d4fde0f14572f
Saidazim008 <saidazibobomurodov@gmail.com>
2026-05-15 13:41:47 +0500

    setup android auto build

---
+++ android/app/src/main/java/com/saidazimxon/valyuta/MainActivity.java
@@ -0,0 +1,5 @@
+package com.saidazimxon.valyuta;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saidazimxon.valyuta',
+  appName: 'Valyuta Kurslari',
+  webDir: 'build'
+};
+
+export default config;
